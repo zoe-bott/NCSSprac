fc9ec22ac297e7c2e18fabe23848f529a7414d3d
zoebott134 <zoe.bott@yahoo.com.au>
2019-01-03 18:43:32 +1100

    made alexa echo

--- hello_world.py
+++ hello_world.py
@@ -21,15 +21,26 @@ def lol():
 
 @app.route('/alexa', methods=['POST', 'GET'])
 def alexa():
-  return jsonify({
-    'version': '0.1',
-    'response': {
-      'outputSpeech': {
-        'type': 'PlainText',
-        'text': 'Hello, welcome to my bot, the zobot!'
-      }
-    }
-  })
+    data = request.get_json()
+
+    try:
+        # The request structure is deeply nested
+        # We expect the requested intent to have a slot called 'Query'
+        query = request.json['request']['intent']['slots']['Query']['value']
+        response_text = 'You said: ' + query
+    except KeyError:
+        response_text = 'Did you say something?'
+
+    return jsonify({
+        'version': '0.1',
+        'response': {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': response_text,
+        },
+        'shouldEndSession': False,  # Keep the app alive
+        }
+    })
 
 
 
