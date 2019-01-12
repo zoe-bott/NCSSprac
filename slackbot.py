692957a82831e05c65ff9f0916fe5450bd30d993
zoebott134 <zoe.bott@yahoo.com.au>
2019-01-12 17:42:48 +1100

    changed input

--- slackbot.py
+++ slackbot.py
@@ -15,11 +15,13 @@ def slack_ask():
     input_text = request.values.get('text')
     if input_text:
         state, data, output_on_input = on_input(state, input_text, data)
+        if output_on_input:
+            output+= output_on_input
     else:
         output_on_enter = on_enter_state(state, data)
 
-    if output_on_enter:
-        output += output_on_enter
+        if output_on_enter:
+            output += output_on_enter
 
     return output
 
